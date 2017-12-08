3e9e7214e9b4853262157f65170cb18a40ea0c46
Li, Zihao <lizihao945@gmail.com>
2017-12-08 00:39:13 -0500

    Badge

--- recipes/badges.py
+++ recipes/badges.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from recipes.models import *
+
 from django.contrib.auth.models import User
 
 def getBadges(User):
-	# Number of posts
+	badges = []
+	# First Recipe
+	if Content.objects.filter(user=User).count() > 0:
+		badges.append("Chef")
+
+	# First down vote
+	for vote in Vote.objects.filter(user=User):
+		if not vote.isUp:
+			badges.append("Critic")
+			break;
+
+	# First up vote
+	for vote in Vote.objects.filter(user=User):
+		if not vote.isUp:
+			badges.append("Supporter")
+			break;
+
+	# Number of votes
+
 	# Number of favorites
-	return
+
+	return badges
